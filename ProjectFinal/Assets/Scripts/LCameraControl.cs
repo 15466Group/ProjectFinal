0fa3f12179fe06cb6a6ddcf6ff544257dba43297
dominicxcliu <dominicxcliu@gmail.com>
2015-12-10 15:37:26 -0500

    fov

--- ProjectFinal/Assets/Scripts/LCameraControl.cs
+++ ProjectFinal/Assets/Scripts/LCameraControl.cs
@@ -4,15 +4,24 @@ using System.Collections;
 public class LCameraControl : MonoBehaviour {
 	
 	public GameObject player;
+	private float ratio;
 	// Use this for initialization
 	void Start () {
 //		transform.position = player.transform.position + Vector3.up * 980f;
 //		transform.LookAt (Vector3.zero);
+		float w = (float)Screen.width;
+		float h = (float)Screen.height;
+		ratio = w / h;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 //		transform.position = player.transform.position + Vector3.up * 980f;
 //		transform.LookAt (Vector3.zero);
+		float w = (float)Screen.width;
+		float h = (float)Screen.height;
+		ratio = w / h;
+		Debug.Log ("RATIO" + ratio);
+		this.GetComponent <Camera> ().fieldOfView = (21f) * (-1f) * ratio + 76f; 
 	}
 }
